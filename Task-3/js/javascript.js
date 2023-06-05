3d83d8976d466f40e382180281e79dfc16e1f62a
P_Al_K <m.pws.g.acc@gmail.com>
2023-06-06 02:07:36 +0500

    commit_1

---
+++ Task-3/js/javascript.js
@@ -0,0 +1,8 @@
+const wsUri = "wss://ws.postman-echo.com/raw";
+
+const input = document.querySelector('.j-input');
+const btnSend = document.querySelector('.j-btn-send');
+const btnGeo = document.querySelector('.j-btn-geo');
+const output = document.querySelector('.output');
+
+
